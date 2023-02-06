d513eaea7597c019a7c4cd4d3c7037dd669901d4
Swatijl <swatisam76@gmail.com>
2023-02-06 21:45:40 +0530

    corrected spelling

--- src/utils/cities.js
+++ src/utils/cities.js
@@ -2,7 +2,7 @@ export const cities = [
   { id: '1', name: 'Mumbai', state: 'Maharashtra' },
   { id: '2', name: 'Delhi', state: 'Delhi' },
   { id: '3', name: 'Bengaluru', state: 'Karnataka' },
-  { id: '4', name: 'Ahmedabad', state: 'Gujarat' },
+  { id: '4', name: 'Ahemdabad', state: 'Gujarat' },
   { id: '5', name: 'Hyderabad', state: 'Telangana' },
   { id: '6', name: 'Chennai', state: 'Tamil Nadu' },
   { id: '7', name: 'Kolkata', state: 'West Bengal' },
